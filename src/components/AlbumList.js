01a92d2f776d6f21594eabe8d1d0f0c4096bcbce
Philip Alekseev <phil.alekseev@gmail.com>
2016-10-09 12:37:48 -0400

    Working with state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,12 +3,15 @@ import { View, Text } from 'react-native';
 import Axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>List of Albums</Text>
