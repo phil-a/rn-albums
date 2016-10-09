a12d958770147892898fb4440966d9d16a2c515c
Philip Alekseev <phil.alekseev@gmail.com>
2016-10-09 12:17:19 -0400

    refactor album list to class-based component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>List of Albums</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>List of Albums</Text>
+      </View>
+    );
+  }
+}
 
-export default AlbumList
+export default AlbumList;
