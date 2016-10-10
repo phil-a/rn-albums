5ea324d89eea9e675d98d508b105f2cca04fd3c8
Philip Alekseev <phil.alekseev@gmail.com>
2016-10-09 22:07:35 -0400

    Add ScrollView + Button Component

---
+++ src/components/Button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+
+const Button = () => {
+  const { buttonStyle, textStyle } = styles;
+
+  return (
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>
+        Click
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingVertical: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginHorizontal: 5
+  }
+};
+
+export default Button;
